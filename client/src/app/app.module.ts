3ad5b9cc40b46aae47cc538694dcf0a3385e5420
wvha <Wvha17@gmail.com>
2019-03-05 23:11:28 -0800

    add new component folders to app

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { VideosComponent } from './videos/videos.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VideosComponent,
+    VideoListComponent,
+    VideoPlayerComponent,
+
   ],
   imports: [
     BrowserModule
